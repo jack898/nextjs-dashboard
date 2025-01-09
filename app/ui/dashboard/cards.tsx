8fe8da6d6daee81f736128fba158901d43028124
Jack Burton <jackb898@gmail.com>
2025-01-09 16:06:11 -0600

    Update cards.tsx

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,12 +16,13 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+
+      <Card title="Collected" value="" type="collected" />
+      <Card title="Pending" value="" type="pending" />
+      <Card title="Total Invoices" value="" type="invoices" />
       <Card
         title="Total Customers"
-        value={numberOfCustomers}
+        value=""
         type="customers"
       />
     </>
