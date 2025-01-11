aa768c23b394e0596b2b5b8164d9c40af44bcc45
Jack Burton <jackb898@gmail.com>
2025-01-11 14:33:04 -0600

    Update edit-form.tsx

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,8 +10,6 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice, State } from '@/app/lib/actions';
-
-'use client';
 import { useActionState } from 'react';
 
 
